b40371bc2bd875ae27255b47b582343b3eeb79ba
Michael Gill <mikegill@icloud.com>
2020-10-07 11:37:27 +0100

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Omnipotent on 07/10/2020.
+//  Copyright © 2020 Omnipotent. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
